e33c8ece9102715b02082c8cb7ef3c6e7e8f5611
Chris Sanders <astericky@gmail.com>
2019-07-21 17:12:08 -0700

    Initial Commit

---
+++ GoodService/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GoodService
+//
+//  Created by Chris Sanders on 7/21/19.
+//  Copyright © 2019 Chris Sanders. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
