e9220de24e928fa46dac7629c7001a39fa6a7337
Chris Sanders <astericky@gmail.com>
2019-10-03 11:17:46 -0700

    add slow zones  view

--- GoodService/Tabs/Slow Zones/SlowZoneList.swift
+++ GoodService/Tabs/Slow Zones/SlowZoneList.swift
@@ -5,25 +5,28 @@
 //  Created by Chris Sanders on 9/17/19.
 //  Copyright © 2019 Chris Sanders. All rights reserved.
 //
-//
-//import SwiftUI
-//
-//struct SlowZoneList: View {
-//    @ObservedObject var routeInfo = RoutesInfoViewModel()
-//    
-//    var body: some View {
-//        Text("Slow Zones")
-////        NavigationView {
-////            List(routeInfo.slowZones, id: \.self) { line in
-////                NavigationLink(destination: LineDetail(line: line)) {
-////                    LineRow(line: line)
-////                }
-////            }
-////            .navigationBarTitle(Text("Slow Zones"))
-////        }
-//    }
-//}
-//
+
+import SwiftUI
+
+struct SlowZoneList: View {
+    @ObservedObject var viewModel: RoutesInfoViewModel
+    
+    var body: some View {
+        NavigationView {
+            List(viewModel.slowZones, id: \.self) { line in
+                Text(line.name)
+//                NavigationLink(destination: LineDetail(item: line)) {
+//                    LineRow(item: line)
+//                }
+            }
+            .navigationBarTitle(Text("Slow Zones"))
+        }
+    }
+    init(viewModel: RoutesInfoViewModel) {
+        self.viewModel = viewModel
+    }
+}
+
 //struct SlowZoneList_Previews: PreviewProvider {
 //    static var previews: some View {
 //        SlowZoneList()
